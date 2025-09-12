711d062c15f34ec48bf27571d8dad2998c2537eb
doof <itrololo1703@gmail.com>
2025-09-12 21:36:29 +0000

    Complete 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!(typeof start === 'number' 
+    && typeof end === 'number' 
+    && Number.isInteger(start)
+    && Number.isInteger(end)
+    && start >= 0
+    && end >= 0)) return 'ERROR'
 
+  let result = 0;
+  
+  if (start < end) {
+    for (let i = start; i <= end; i++) result += i;
+  } else {
+    for (let i = end; i <= start; i++) result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
