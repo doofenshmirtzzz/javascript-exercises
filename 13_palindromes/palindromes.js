863f8f34ad404778fddf1cd8c921754cdba3d12d
doof <itrololo1703@gmail.com>
2025-10-19 21:33:00 +0000

    Complete 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let normString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  let reversedString = normString.split('').reverse().join('');
+  return normString === reversedString;
 };
 
 // Do not edit below this line
