3c49281a79ac4c4a5eb2f7d13b9decfdf64d10eb
doof <itrololo1703@gmail.com>
2025-10-20 16:27:22 +0000

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  const num = Number(n);
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
 
+  let prev = 0, curr = 1;
+
+  for(let i = 1; i < num; i++) {
+    next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
