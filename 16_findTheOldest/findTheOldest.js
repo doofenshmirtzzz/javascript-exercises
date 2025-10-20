ce17f0f6d7766323d846adb415c0d5c1810e0c8f
doof <itrololo1703@gmail.com>
2025-10-20 17:11:52 +0000

    Complete 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
 
+  let oldest = arr[0];
+
+  function getAge(e) {
+    if ('yearOfDeath' in e) {
+      return e.yearOfDeath - e.yearOfBirth;
+    } else {
+      return currentYear - e.yearOfBirth;
+    }
+  }
+
+  for (e of arr) {
+    if (getAge(oldest) < getAge(e)) oldest = e;
+  }
+
+  return oldest;
 };
 
 // Do not edit below this line
